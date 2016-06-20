09a79dcaefb5eac0622659ad2f5d47d1a14f69af
Koule7b <stepan.mudra@gamil.com>
2016-06-20 07:17:46 +0200

    kodovani ,  dekodovani

--- src/Kodovani.java
+++ src/Kodovani.java
@@ -1,13 +1,16 @@
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Admin on 19.6.2016.
  */
 public class Kodovani {
     static char[] velkyZnaky = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    static char[] malyZnaky = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    static char[] nulaJedna = "01".toCharArray();
+    static char[] kodyPismen = new char[velkyZnaky.length];
     static ArrayList<Object> pruchozi = new ArrayList<>();
+    static ArrayList<Object> kod = new ArrayList<>();
     static int[] pocty = new int[velkyZnaky.length];
     static int celkovyPOcetZnaku;
     static double E = 0;
@@ -15,11 +18,15 @@ public class Kodovani {
 
     public static void main(String[] args) {
         nacteniTextuZeSouboru();
-        pruchod(pruchozi, velkyZnaky, malyZnaky);
+        pruchod(pruchozi, velkyZnaky);
         pravdepodobnosti();
         entropie();
         serazeniPravdepodobnosti();
+        pruchodSKodovanim(pruchozi, velkyZnaky);
+        System.out.println();
+        pruchodSDekodovanim(kod);
     }
+
     static void nacteniTextuZeSouboru() {
         try (FileReader br = new FileReader("soubor.txt")) {
             char s;
@@ -34,12 +41,13 @@ public class Kodovani {
             System.err.println("Chyba při četení ze souboru.");
         }
     }
-    static void pruchod(ArrayList<?> text, char[] prvky, char[] prvkyMaly) {
+
+    static void pruchod(ArrayList<?> text, char[] prvky) {
         for (int i = 0; i < velkyZnaky.length; i++) {
             int pocetVyskytu = 0;
             System.out.print(velkyZnaky[i] + " ");
             for (int k = 0; k < text.size(); k++) {
-                if (String.valueOf(prvky[i]).contains(String.valueOf(text.get(k))) || String.valueOf(prvkyMaly[i]).contains(String.valueOf(text.get(k)))) {
+                if (String.valueOf(prvky[i]).toLowerCase().contains(String.valueOf(text.get(k)).toLowerCase())) {
                     pocetVyskytu++;
                 }
             }
@@ -47,18 +55,21 @@ public class Kodovani {
             System.out.println(pocetVyskytu);
         }
     }
-    private static void pravdepodobnosti(){
+
+    private static void pravdepodobnosti() {
         for (int i = 0; i < pravdepodobnostPismen.length; i++) {
-            pravdepodobnostPismen[i] = (double)pocty[i] / (double)celkovyPOcetZnaku;
-            System.out.println("Pravdepodobnost"+velkyZnaky[i]+" je "+(double)pocty[i] / (double)celkovyPOcetZnaku);
+            pravdepodobnostPismen[i] = (double) pocty[i] / (double) celkovyPOcetZnaku;
+            System.out.println("Pravdepodobnost" + velkyZnaky[i] + " je " + (double) pocty[i] / (double) celkovyPOcetZnaku);
         }
     }
-    private static void entropie(){
+
+    private static void entropie() {
         for (int i = 0; i < pocty.length; i++) {
             E = E - (pravdepodobnostPismen[i] * (Math.log(pravdepodobnostPismen[i]) / Math.log(2)));
         }
-        System.out.println("Entropie je "+E);
+        System.out.println("Entropie je " + E);
     }
+
     public static void serazeniPravdepodobnosti() {
         double pomoc;
         char pomoc1;
@@ -74,6 +85,41 @@ public class Kodovani {
                 }
             }
         }
-
+        //System.out.println(Arrays.toString(pravdepodobnostPismen));
+        //System.out.println(Arrays.toString(velkyZnaky));
+    }
+    private static void pruchodSKodovanim(ArrayList<?> text, char[] prvky){
+            for (int k = 0; k < text.size(); k++) {
+                for (int i = 0; i < velkyZnaky.length; i++) {
+                    if(String.valueOf(velkyZnaky[i]).toLowerCase().contains(String.valueOf(text.get(k)).toLowerCase())){
+                        kodovani(pravdepodobnostPismen[i]);
+                    }
+                }
+            }
+    }
+    private static void kodovani(double pravdepodobnostPismene) {
+        int k = 0;
+        for (int i = 0; i < pravdepodobnostPismen.length; i++) {
+            if (pravdepodobnostPismene < pravdepodobnostPismen[i]) {
+                k++;
+            }
+        }
+        for (int j = 0; j < k; j++) {
+            kod.add(0);
+            System.out.print(0);
+        }
+        kod.add(1);
+        System.out.print(1);
+    }
+    private static void pruchodSDekodovanim(ArrayList<?> kod){
+        int k = 0;
+        for (int i = 0; i < kod.size(); i++) {
+            if(String.valueOf(kod.get(i)).contains(String.valueOf(nulaJedna[1]))){
+                System.out.print(velkyZnaky[k]);
+                k = 0;
+            }else{
+                k++;
+            }
+        }
     }
 }
